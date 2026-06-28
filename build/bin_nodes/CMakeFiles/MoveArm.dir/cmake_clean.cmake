3002f262a594aa91dda850fe34845888feb84d49
Ibrahim Zantout <kingofdeath3636@gmail.com>
2026-06-28 03:10:02 +0300

    init the ros2 proj

---
+++ build/bin_nodes/CMakeFiles/MoveArm.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MoveArm.dir/src/MoveArm.cpp.o"
+  "CMakeFiles/MoveArm.dir/src/MoveArm.cpp.o.d"
+  "MoveArm"
+  "MoveArm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MoveArm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
